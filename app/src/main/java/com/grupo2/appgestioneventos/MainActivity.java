36ee9a5c2b61775bc5de77703d48a39b11a1b3a5
Agerzace <103101643+aitor-martinezur@users.noreply.github.com>
2022-09-29 13:47:50 +0200

    Upd Aitor/Loading_menu

--- app/src/main/java/com/grupo2/appgestioneventos/MainActivity.java
+++ app/src/main/java/com/grupo2/appgestioneventos/MainActivity.java
@@ -3,12 +3,21 @@ package com.grupo2.appgestioneventos;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.Window;
+import android.view.WindowManager;
+import android.widget.ProgressBar;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        this.setContentView(R.layout.activity_main);
+
+        //crea la variable para la progressbar
+        final ProgressBar progressBar = findViewById(R.id.progressBar3);
+        //la pone visible
+        progressBar.getProgress();
+
     }
 }
