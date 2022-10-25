63346ea67a5a0705110fd8fa9db34286cd7bcfbc
Tecleador <jorge.lopezsa@elorrieta-errekamari.com>
2022-10-25 14:24:19 +0200

    Cambios pequeños para probar cosas

--- app/src/main/java/com/grupo2/appgestioneventos/AdminContactoActivity.java
+++ app/src/main/java/com/grupo2/appgestioneventos/AdminContactoActivity.java
@@ -7,25 +7,25 @@ import android.widget.Button;
 
 
 import java.util.ArrayList;
-public class AdminContactoActivity extends MenuActivity {
+/*public class AdminContactoActivity extends MenuActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        this.setContentView(R.layout.admincontact);
+             this.setContentView(R.layout.admincontact);
 
-       // ArrayList<Contactos> contactos = new ArrayList<>();
+       ArrayList<Contactos> contactos = new ArrayList<>();
         Bundle extras = getIntent().getExtras();
         if (extras!=null) {
-        //    contactos = (ArrayList<Contactos>) getIntent().getExtras().getSerializable("keyContactos");
+            contactos = (ArrayList<Contactos>) getIntent().getExtras().getSerializable("keyContactos");
         }
         Button botonAnadirContacto = findViewById(R.id.);
-        //ArrayList<Usuario> finalUsuarios1 = usuarios;
+        ArrayList<Usuario> finalUsuarios1 = usuarios;
         botonAnadirContacto.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                //pasa los valores a la siguiente actividad y la inicia
-               // Intent k = new Intent(AdminUsersActivity.this, AnadirUsuarioActivity.class);
-                //k.putExtra("keyUsuarios", finalUsuarios1);
+                pasa los valores a la siguiente actividad y la inicia
+                Intent k = new Intent(AdminUsersActivity.this, AnadirUsuarioActivity.class);
+                k.putExtra("keyUsuarios", finalUsuarios1);
                 startActivity(k);
             }
         });
@@ -33,3 +33,4 @@ public class AdminContactoActivity extends MenuActivity {
     }
 
 }
+*/
