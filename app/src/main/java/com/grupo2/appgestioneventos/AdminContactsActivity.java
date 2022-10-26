c7ef2c76df5ce6ff6c1bda1e5c031ef8c4bb789e
Tecleador <jorge.lopezsa@elorrieta-errekamari.com>
2022-10-26 08:32:23 +0200

    Navegación de contact
    Navegación y algunos cambios en java.
    Todavia no funciona del todo

--- app/src/main/java/com/grupo2/appgestioneventos/AdminContactsActivity.java
+++ app/src/main/java/com/grupo2/appgestioneventos/AdminContactsActivity.java
@@ -5,11 +5,40 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
+import java.util.ArrayList;
+
 public class AdminContactsActivity extends MenuActivity{
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         this.setContentView(R.layout.admincontact);
+
+        //funcionalidad boton anadir
+        Button botonAnadir = findViewById(R.id.AddContact);
+        botonAnadir.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(AdminContactsActivity.this, AnadirContactoActivity.class));
+            }
+        });
+
+        //funcionalidad boton actualizar
+        Button botonActualizar = findViewById(R.id.UpdateContact);
+        botonActualizar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(AdminContactsActivity.this, ActualizarContactoActivity.class));
+            }
+        });
+
+        //funcionalidad boton borrar
+        Button botonBorrar = findViewById(R.id.DeleteContact);
+        botonBorrar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(AdminContactsActivity.this, BorrarContactoActivity.class));
+            }
+        });
 
     }
 }
--- app/src/main/java/com/grupo2/appgestioneventos/AñadirContactoActivity.java
+++
@@ -1,5 +0,0 @@
-package com.grupo2.appgestioneventos;
-
-public class AñadirContactoActivity {
-
-}
